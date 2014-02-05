dc0cdcdba5003d2f354d2716f89060cebe5cb570
Clark Perkins <r.clark.perkins@gmail.com>
2014-02-05 00:06:25 -0600

    Added ‘blur’ effect

--- Music City Center/MCCMapViewController.m
+++ Music City Center/MCCMapViewController.m
@@ -24,16 +24,30 @@ static NSString * const identifier = @"Cell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.contents = [NSArray arrayWithObjects:@"Davidson Ballroom", @"Grand Ballroom", @"Room 101", nil];
+    self.contents = [NSArray arrayWithObjects:@"Grand Ballroom", @"L4 Balcony", @"Room 401", @"Room 402", @"Room 403",
+                     @"Hall A1", @"Hall A2", @"Hall B", @"Hall C", @"Hall D",
+                     @"L3 Terrace", @"Lounge",
+                     @"L2 Balcony",
+                     @"Room 201", @"Room 202", @"Room 203", @"Room 204", @"Room 205", @"Room 206", @"Room 207", @"Room 208", @"Room 209", @"Room 210", @"Room 211", @"Room 212", @"Room 213", @"Room 214",
+                     @"Davidson Ballroom", @"Board Room A", @"Board Room B", @"L1M Balcony",
+                     @"L1 Terrace",
+                     @"Room 101", @"Room 102", @"Room 103", @"Room 104", @"Room 105", @"Room 106", @"Room 107", @"Room 108", @"Room 109", @"Room 110",
+                     nil];
     
     
     [self.searchDisplayController.searchResultsTableView registerClass:[UITableViewCell class]
                                                 forCellReuseIdentifier:identifier];
     
-    self.searchDisplayController.searchResultsTableView.alpha = 0.7;
-    
+    // Put the search bar in the nav bar
     [self.searchDisplayController.searchBar removeFromSuperview];
     self.searchDisplayController.displaysSearchBarInNavigationBar = YES;
+    
+    // Set the tableview background to clear so the toolbar can be seen underneath
+    self.searchDisplayController.searchResultsTableView.backgroundColor = [UIColor clearColor];
+    
+    // Set the table background view to be a toolbar - the toolbar provides the 'blur' effect
+    // **** This is kind of a hack, so I'm not sure if we should actually do this ****
+    self.searchDisplayController.searchResultsTableView.backgroundView = [[UIToolbar alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,6 +56,7 @@ static NSString * const identifier = @"Cell";
     // Dispose of any resources that can be recreated.
 }
 
+// Find all matching strings
 - (void)findMatches {
     self.searchContents = [[NSMutableArray alloc] init];
     for (NSString* str in self.contents) {
@@ -51,7 +66,6 @@ static NSString * const identifier = @"Cell";
         }
     }
 }
-
 
 #pragma mark - Table View Data Source
 
@@ -67,6 +81,9 @@ static NSString * const identifier = @"Cell";
                                                             forIndexPath:indexPath];
     
     cell.textLabel.text = [self.searchContents objectAtIndex:indexPath.row];
+    
+    // Set the backround to clear so you can see the blur effect underneath
+    cell.backgroundColor = [UIColor clearColor];
     
     return cell;
 }
