6cbb70ef177480a9bb0bd44ccb0d4e38b144cf36
Seth Friedman <sethfri@gmail.com>
2014-02-26 02:36:27 -0600

    Add designated initializer and factory method to MCCNavPath

--- Music City Center/MCCNavPath.m
+++ Music City Center/MCCNavPath.m
@@ -10,4 +10,22 @@
 
 @implementation MCCNavPath
 
+#pragma mark - Designated Initializer
+
+- (instancetype)initWithEdges:(NSArray *)edges {
+    self = [super init];
+    
+    if (self) {
+        _edges = edges;
+    }
+    
+    return self;
+}
+
+#pragma mark - Factory Method
+
++ (instancetype)navPathWithEdges:(NSArray *)edges {
+    return [[self alloc] initWithEdges:edges];
+}
+
 @end
