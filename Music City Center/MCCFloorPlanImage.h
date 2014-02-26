925a27f982802ecfd42fb343430c747867a93978
Clark Perkins <r.clark.perkins@gmail.com>
2014-02-26 17:10:49 -0600

    Added MCCFloorPlanImage class that has a method to turn image coordinates into lat/long

---
+++ Music City Center/MCCFloorPlanImage.h
@@ -0,0 +1,27 @@
+//
+//  MCCFloorPlanImage.h
+//  Music City Center
+//
+//  Created by Clark Perkins on 2/26/14.
+//  Copyright (c) 2014 Music City Center. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "MCCFloorplanImageLocation.h"
+
+@interface MCCFloorPlanImage : NSObject
+
+@property (nonatomic) NSInteger sizeX;
+@property (nonatomic) NSInteger sizeY;
+
+@property (nonatomic) CLLocationCoordinate2D topLeft;
+@property (nonatomic) CLLocationCoordinate2D topRight;
+@property (nonatomic) CLLocationCoordinate2D bottomLeft;
+
+
+- (instancetype)initWithsizeX:(NSInteger)x sizeY:(NSInteger)y topLeft:(CLLocationCoordinate2D)tl topRight:(CLLocationCoordinate2D)tr bottomLeft:(CLLocationCoordinate2D)bl;
+
+- (CLLocationCoordinate2D)getLatLongFromLocation:(MCCFloorplanImageLocation *)loc;
+
+@end
