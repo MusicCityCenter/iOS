cf987a3eb7637b533eac6b43109004cfa5956e1d
Clark Perkins <r.clark.perkins@gmail.com>
2014-03-11 22:34:43 -0500

    Changed deprecated OverlayView delegate method to OverlayRenderer

--- Music City Center/MCCFloorViewController.m
+++ Music City Center/MCCFloorViewController.m
@@ -38,6 +38,8 @@
     
     self.mapView.region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(36.1575, -86.777), MKCoordinateSpanMake(.004, .004));
 
+    self.mapView.delegate = self;
+    
 //    self.mapView.mapType = MKMapTypeHybrid;
     
     self.floor1TopLeft = [MCCFloorPlanImageLocation floorPlanImageLocationWithX:240 andY:3196];
@@ -63,7 +65,7 @@
     [client fetchFloorPlan:@"full-test-1" withCompletionBlock:^(MCCNavData *navData) {
         // Get the mapping from the navData
     
-        [client shortestPathOnFloorPlan:@"full-test-1" from:@"B-1-2" to:event.locationId withCompletionBlock:^(MCCNavPath *path) {
+        [client shortestPathOnFloorPlan:@"full-test-1" from:@"110" to:event.locationId withCompletionBlock:^(MCCNavPath *path) {
             
             // Get the number of points, which is the number of edges + 1
             NSUInteger numPoints = [path.edges count] + 1;
@@ -73,11 +75,13 @@
             
             // Start with the start of the first edge
             MCCFloorPlanEdge *edge0 = [path.edges firstObject];
+            NSLog(@"%@",edge0.startLocation.locationId);
             MCCFloorPlanImageLocation *location0 = [navData.mapping coordinatesOfLocation:edge0.startLocation.locationId];
             
             MCCFloorPlanImageLocation *translatedLocation0 =
             [MCCFloorPlanImageLocation floorPlanImageLocationWithX:location0.x - self.floor1TopLeft.x
                                                               andY:location0.y - self.floor1TopLeft.y];
+            
             
             // Turn the floorplan location into lat-long
             coords[0] = [self.floor1 coordinateFromFloorPlanImageLocation:translatedLocation0];
@@ -88,19 +92,15 @@
             for (MCCFloorPlanEdge *edge in path.edges) {
                 MCCFloorPlanImageLocation *location = [navData.mapping coordinatesOfLocation:edge.endLocation.locationId];
                 
+                NSLog(@"%@",edge.endLocation.locationId);
+                
                 MCCFloorPlanImageLocation *translatedLocation =
                 [MCCFloorPlanImageLocation floorPlanImageLocationWithX:location.x - self.floor1TopLeft.x
                                                                   andY:location.y - self.floor1TopLeft.y];
                 
-                NSLog(@"(%lu, %lu)", translatedLocation.x, translatedLocation.y);
-                
                 // Turn the floorplan location into lat-long
                 coords[i] = [self.floor1 coordinateFromFloorPlanImageLocation:translatedLocation];
                 ++i;
-            }
-            
-            for (int j = 0; j < numPoints; ++j) {
-                NSLog(@"(%f, %f)", coords[j].latitude, coords[j].longitude);
             }
             
             self.polyline = [MKPolyline polylineWithCoordinates:coords count:numPoints];
@@ -117,13 +117,17 @@
 
 #pragma mark - MKMapViewDelegate
 
-- (MKOverlayView*)mapView:(MKMapView*)theMapView viewForOverlay:(id <MKOverlay>)overlay
-{
-    MKPolylineView* lineView = [[MKPolylineView alloc] initWithPolyline:self.polyline];
-    lineView.fillColor = [UIColor redColor];
-    lineView.strokeColor = [UIColor redColor];
-    lineView.lineWidth = 4;
-    return lineView;
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    if ([overlay isKindOfClass:[MKPolyline class]]) {
+        MKPolyline *route = overlay;
+        MKPolylineRenderer *routeRenderer = [[MKPolylineRenderer alloc] initWithPolyline:route];
+        routeRenderer.fillColor = [UIColor redColor];
+        routeRenderer.strokeColor = [UIColor redColor];
+        routeRenderer.lineWidth = 4;
+        return routeRenderer;
+    } else {
+        return nil;
+    }
 }
 
 @end
