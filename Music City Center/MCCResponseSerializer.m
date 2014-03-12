4b4a01d3add97a6a93da1d6bd832aae54f1e5940
Seth Friedman <sethfri@gmail.com>
2014-03-11 20:44:18 -0500

    Add response serialization for path

--- Music City Center/MCCResponseSerializer.m
+++ Music City Center/MCCResponseSerializer.m
@@ -7,6 +7,10 @@
 //
 
 #import "MCCResponseSerializer.h"
+#import "MCCFloorPlan.h"
+#import "MCCFloorPlanLocation.h"
+#import "MCCFloorPlanEdge.h"
+#import "MCCNavPath.h"
 #import "MCCEvent.h"
 
 @implementation MCCResponseSerializer
@@ -32,21 +36,39 @@
     
     // responseObject = floorPlan;
     
-    // Example FloorPlan Mapping Response:
-    //{"mapping":{"imageUrl":"/mcc/image/floorplan/windsor","mapping":{"k":{"x":878,"y":556},"g":{"x":679,"y":541},"f":{"x":477,"y":524},"df":{"x":880,"y":359}}},"floorplan":{"locations":[{"id":"g","type":"room"},{"id":"df","type":"room"},{"id":"k","type":"room"},{"id":"f","type":"room"}],"edges":[{"start":"g","end":"k","length":0.0},{"start":"g","end":"f","length":0.0},{"start":"df","end":"k","length":0.0},{"start":"k","end":"g","length":0.0},{"start":"k","end":"df","length":0.0},{"start":"f","end":"g","length":0.0}],"types":{"name":"root","children":[{"name":"room"}]}}}
-    
-    // Example Path Response:
-    // [{"from":"f","to":"g","length":202.71408436514716,"angle":355.18941380563433},{"from":"g","to":"k","length":199.56452590578317,"angle":355.68937417517947}]
-    
-    // Example Events Response:
-    // [{"id":"75cc1e2b-b26c-4668-83b1-99433f4d334f","name":"asdf","description":"asdf","day":5,"month":1,"year":2014,"startTime":420,"endTime":1380,"floorplanId":"windsor","floorplanLocationId":"g"}]
-    
     NSURL *relativeURL = [NSURL URLWithString:response.URL.relativePath];
     NSString *firstPathComponent = [relativeURL.pathComponents firstObject];
     
-    // TODO - Change the order of these checks to floorplan, then shortest path, then events - just for consistency - instead of
-    // events first
-    if ([firstPathComponent isEqualToString:@"events"]) {
+    if ([firstPathComponent isEqualToString:@"floorplan"]) {
+        // Example FloorPlan Mapping Response:
+        //{"mapping":{"imageUrl":"/mcc/image/floorplan/windsor","mapping":{"k":{"x":878,"y":556},"g":{"x":679,"y":541},"f":{"x":477,"y":524},"df":{"x":880,"y":359}}},"floorplan":{"locations":[{"id":"g","type":"room"},{"id":"df","type":"room"},{"id":"k","type":"room"},{"id":"f","type":"room"}],"edges":[{"start":"g","end":"k","length":0.0},{"start":"g","end":"f","length":0.0},{"start":"df","end":"k","length":0.0},{"start":"k","end":"g","length":0.0},{"start":"k","end":"df","length":0.0},{"start":"f","end":"g","length":0.0}],"types":{"name":"root","children":[{"name":"room"}]}}}
+        
+        
+    } else if ([firstPathComponent isEqualToString:@"path"]) {
+        // Example Path Response:
+        // [{"from":"f","to":"g","length":202.71408436514716,"angle":355.18941380563433},{"from":"g","to":"k","length":199.56452590578317,"angle":355.68937417517947}]
+        
+        NSMutableArray *floorPlanEdges = [NSMutableArray arrayWithCapacity:[responseObject count]];
+        
+        for (NSDictionary *edgeDictionary in responseObject) {
+            MCCFloorPlanLocation *startLocation = [MCCFloorPlanLocation floorPlanLocationWithLocationId:edgeDictionary[@"from"]
+                                                                                                andType:@""];
+            MCCFloorPlanLocation *endLocation = [MCCFloorPlanLocation floorPlanLocationWithLocationId:edgeDictionary[@"to"]
+                                                                                              andType:@""];
+            
+            MCCFloorPlanEdge *edge = [MCCFloorPlanEdge floorPlanEdgeWithStartLocation:startLocation
+                                                                          endLocation:endLocation
+                                                                               length:[edgeDictionary[@"length"] floatValue]
+                                                                             andAngle:[edgeDictionary[@"angle"] floatValue]];
+            
+            [floorPlanEdges addObject:edge];
+        }
+        
+        responseObject = [MCCNavPath navPathWithEdges:[floorPlanEdges copy]];
+    } else if ([firstPathComponent isEqualToString:@"events"]) {
+        // Example Events Response:
+        // [{"id":"75cc1e2b-b26c-4668-83b1-99433f4d334f","name":"asdf","description":"asdf","day":5,"month":1,"year":2014,"startTime":420,"endTime":1380,"floorplanId":"windsor","floorplanLocationId":"g"}]
+        
         NSMutableArray *events = [NSMutableArray arrayWithCapacity:[responseObject count]];
         
         for (NSDictionary *eventDictionary in responseObject) {
@@ -58,6 +80,8 @@
             
             [events addObject:event];
         }
+        
+        responseObject = [events copy];
     }
     
     return responseObject;
