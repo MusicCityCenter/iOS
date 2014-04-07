fd99f9465a2d5720a16c57dffc3a1d1c5b0fdf7c
Clark Perkins <r.clark.perkins@gmail.com>
2014-04-07 18:08:51 -0500

    Created stub methods for everything

--- Music City Center/MCCSearchDisplayController.h
+++ Music City Center/MCCSearchDisplayController.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MCCSearchDisplayController : UISearchDisplayController
+@interface MCCSearchDisplayController : NSObject
+
+@property(nonatomic, getter=isActive) BOOL active;
+@property(nonatomic, assign) id<UISearchDisplayDelegate> delegate;
+@property(nonatomic, assign) BOOL displaysSearchBarInNavigationBar;
+@property(nonatomic, readonly) UINavigationItem *navigationItem;
+@property(nonatomic, readonly) UISearchBar *searchBar;
+@property(nonatomic, readonly) UIViewController *searchContentsController;
+@property(nonatomic, assign) id<UITableViewDataSource> searchResultsDataSource;
+@property(nonatomic, assign) id<UITableViewDelegate> searchResultsDelegate;
+@property(nonatomic, readonly) UITableView *searchResultsTableView;
+@property(nonatomic, copy) NSString *searchResultsTitle;
+
+- (id)initWithSearchBar:(UISearchBar *)searchBar contentsController:(UIViewController *)viewController;
+- (void)setActive:(BOOL)visible animated:(BOOL)animated;
 
 @end
