738ea716a7f134210a8cd19e4f31f06d0e9dee41
Seth Friedman <sethfri@gmail.com>
2014-04-15 04:57:50 -0500

    Remove unnecessary initializer from Event Detail VC

--- Music City Center/MCCEventDetailViewController.m
+++ Music City Center/MCCEventDetailViewController.m
@@ -23,15 +23,6 @@
 
 @implementation MCCEventDetailViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
